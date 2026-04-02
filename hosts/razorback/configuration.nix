da35460685222ebc397f6fa7d488054ffa2f04c7
thiago <thiagokoster@gmail.com>
2026-04-03 00:38:53 +0200

    more changes

--- hosts/razorback/configuration.nix
+++ hosts/razorback/configuration.nix
@@ -96,10 +96,6 @@
      vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
      ffmpeg
   #  wget
-     
-     # Niri initial setup
-     alacritty
-     fuzzel
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
--- modules/niri/default.nix
+++ modules/niri/default.nix
@@ -6,6 +6,12 @@
         ../waybar.nix
     ];
 
+	home.packages = with pkgs; [
+        kitty
+        fuzzel
+        xwayland-satellite
+	];
+
     xdg.configFile."niri/config.kdl".source = ./config.kdl;
 
 }
